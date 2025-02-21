982aec26893630c63b0720f5a3c79d4c5d3ffa43
sgkochar <sgkochar@gmail.com>
2025-02-21 12:32:02 +0200

    add Achievements and Awards blocks to About page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './css/About.css'
-import { MembersItem, MembersList } from '../components'
+import { Faq, MembersItem, MembersList, Testimonials } from '../components'
 
 const About = () => {
     return (
@@ -58,7 +58,7 @@ const About = () => {
                 </div>
                 <div className="about-members-box d-f">
                     {
-                        MembersList.map((member)=> {
+                        MembersList.map((member) => {
                             return (
                                 <MembersItem
                                     key={member.id}
@@ -69,6 +69,113 @@ const About = () => {
                     }
                 </div>
             </section>
+            <section className='about-achievements'>
+                <h2 className="page-bl-title">Our Achievements</h2>
+                <div className="about-achievements-box d-f">
+                    <div className="about-achievements-item">
+                        <div className="achievements-item-date">September 2023</div>
+                        <h3 className="achievements-item-title">Global Recognition for Innovation</h3>
+                        <p className="achievements-item-desc">
+                            In 2023, our digital agency gained global acclaim for innovative solutions,
+                            setting new standards in the ever-evolving digital landscape.
+                        </p>
+                    </div>
+                    <div className="about-achievements-item">
+                        <div className="achievements-item-date">March 2019</div>
+                        <h3 className="achievements-item-title">Industry Leadership Acknowledged</h3>
+                        <p className="achievements-item-desc">
+                            Recognized as an industry leader in 2019, our agency received prestigious awards,
+                            affirming our commitment to excellence and client satisfaction.
+                        </p>
+                    </div>
+                    <div className="about-achievements-item">
+                        <div className="achievements-item-date">August 2015</div>
+                        <h3 className="achievements-item-title">Expansion into International Markets</h3>
+                        <p className="achievements-item-desc">
+                            Expanding horizons in 2015, our agency successfully entered international markets,
+                            establishing a global presence and serving clients on a broader scale.
+                        </p>
+                    </div>
+                    <div className="about-achievements-item">
+                        <div className="achievements-item-date">January 2010</div>
+                        <h3 className="achievements-item-title">Pioneering the Digital <br /> Frontier</h3>
+                        <p className="achievements-item-desc">
+                            In 2010, our agency was born, pioneering the digital frontier and
+                            establishing core principles that have guided us through a decade of innovation.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <section className='page-bl'>
+                <h2 className="page-bl-title">Awards & Recognitions</h2>
+                <div className="about-awards-box d-f">
+                    <div className="about-awards-item">
+                        <div className="d-f jc-sb align-center">
+                            <div className="awards-item-date d-f align-center">
+                                <div className="date-title">Date</div>
+                                <div><img src="/images/shape.png" alt="shape" /></div>
+                                <div className="date-value">October 2017</div>
+                            </div>
+                            <div><img src="/images/award.png" alt="Award" /></div>
+                        </div>
+                        <h3 className="awards-item-title">Digital Excellence Award</h3>
+                        <p className="awards-item-desc">
+                            Recognition for outstanding contributions to the digital industry,
+                            celebrating our ability to deliver exceptional web design and development
+                            solutions that push the boundaries of creativity and functionality.
+                        </p>
+                    </div>
+                    <div className="about-awards-item">
+                        <div className="d-f jc-sb align-center">
+                            <div className="awards-item-date d-f align-center">
+                                <div className="date-title">Date</div>
+                                <div><img src="/images/shape.png" alt="shape" /></div>
+                                <div className="date-value">March 2019</div>
+                            </div>
+                            <div><img src="/images/award.png" alt="Award" /></div>
+                        </div>
+                        <h3 className="awards-item-title">Top Mobile App Development Agency</h3>
+                        <p className="awards-item-desc">
+                            Recognized as a top mobile app development agency by industry experts,
+                            highlighting our proficiency in delivering seamless and user-centric mobile applications.
+                        </p>
+                    </div>
+                    <div className="about-awards-item">
+                        <div className="d-f jc-sb align-center">
+                            <div className="awards-item-date d-f align-center">
+                                <div className="date-title">Date</div>
+                                <div><img src="/images/shape.png" alt="shape" /></div>
+                                <div className="date-value">July 2022</div>
+                            </div>
+                            <div><img src="/images/award.png" alt="Award" /></div>
+                        </div>
+                        <h3 className="awards-item-title">Best Digital Marketing Campaign</h3>
+                        <p className="awards-item-desc">
+                            Awarded for an exceptional digital marketing campaign with outstanding
+                            results, showcasing our data-driven strategies and targeted marketing
+                            efforts that achieved remarkable business growth for our clients.
+                        </p>
+                    </div>
+                    <div className="about-awards-item">
+                        <div className="d-f jc-sb align-center">
+                            <div className="awards-item-date d-f align-center">
+                                <div className="date-title">Date</div>
+                                <div><img src="/images/shape.png" alt="shape" /></div>
+                                <div className="date-value">November 2024</div>
+                            </div>
+                            <div><img src="/images/award.png" alt="Award" /></div>
+                        </div>
+                        <h3 className="awards-item-title">Innovative Tech Startup Award</h3>
+                        <p className="awards-item-desc">
+                            Recognition of our pioneering efforts as a technology startup, 
+                            acknowledging our commitment to exploring and implementing cutting-edge 
+                            technologies to drive innovation in the digital space.
+                        </p>
+                    </div>
+                </div>
+            </section>
+            <Testimonials/>
+            <Faq/>
         </>
     )
 }
